cfa65ce13d2d56183dcec0c26b6b07558cd3496b
Raphaël Lustin <raphael@lustin.fr>
2026-03-04 21:10:52 +0100

    feat: add French localization

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,8 +1,9 @@
 import { I18n } from "i18n-js";
 import { getLocales } from "expo-localization";
 import en from "@/locales/en.json";
+import fr from "@/locales/fr.json";
 
-const i18n = new I18n({ en });
+const i18n = new I18n({ en, fr });
 i18n.locale = getLocales()[0]?.languageCode ?? "en";
 i18n.defaultLocale = "en";
 i18n.enableFallback = true;
